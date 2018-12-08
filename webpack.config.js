4820754b95ea3b9bb1ec9817ab14457dee9c9977
Rodwyn <rodwynmoreno@gmail.com>
2018-12-07 18:13:37 -0600

    Added config files.

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+
+module.exports = {
+  entry: { main: './src/scripts/index.js'},
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+          'eslint-loader',
+        ]
+      },
+      {
+        test: /\.scss$/,
+        exclude: /dist/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'postcss-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'style.css'
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new StyleLintPlugin({})
+  ]
+};
