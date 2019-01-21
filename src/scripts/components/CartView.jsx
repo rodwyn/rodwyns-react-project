776b8998b18ab32d2d4c12eb2d63b3fa495a5bb2
Rodwyn <rodwynmoreno@gmail.com>
2019-01-21 12:48:48 -0600

    Added cart summary component.

---
+++ src/scripts/components/CartView.jsx
@@ -0,0 +1,21 @@
+import CartSummary from './CartSummary.jsx';
+import React from 'react';
+
+const CartView = () => {
+  return (
+    <div className="container-gap page">
+      <div className="col-12">
+        <h2>Shopping Cart</h2>
+      </div>
+
+      <div className="col-12 col-lg-7">
+        bbvbvbv
+      </div>
+      <div className="col-12 col-lg-5">
+        <CartSummary />
+      </div>
+    </div>
+  );
+};
+
+export default CartView;
