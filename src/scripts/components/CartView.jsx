db2ccd2ed697ed143eecd19359fd280e39bbf449
Rodwyn <rodwynmoreno@gmail.com>
2019-01-22 17:52:36 -0600

    Added cart view.

--- src/scripts/components/CartView.jsx
+++ src/scripts/components/CartView.jsx
@@ -7,9 +7,29 @@ const CartView = () => {
       <div className="col-12">
         <h2>Shopping Cart</h2>
       </div>
-
       <div className="col-12 col-lg-7">
-        bbvbvbv
+        <div className="cart-table">
+          <div className="cart-table-row container-gap">
+            <div className="col-6 col-sm-3">
+              <img src="https://colorlib.com/preview/theme/amado/img/bg-img/cart1.jpg" />
+            </div>
+            <div className="col-6 col-sm-3">
+              <h5>White Modern Chair</h5>
+            </div>
+            <div className="col-6 col-sm-3">$130</div>
+            <div className="col-6 col-sm-3">quantity</div>
+          </div>
+          <div className="cart-table-row container-gap">
+            <div className="col-6 col-sm-3">
+              <img src="https://colorlib.com/preview/theme/amado/img/bg-img/cart1.jpg" />
+            </div>
+            <div className="col-6 col-sm-3">
+              <h5>White Modern Chair</h5>
+            </div>
+            <div className="col-6 col-sm-3">$130</div>
+            <div className="col-6 col-sm-3">quantity</div>
+          </div>
+        </div>
       </div>
       <div className="col-12 col-lg-5">
         <CartSummary />
